9b8074f5e52899e2fdc5a3015c1353c59f22c114
collegeeventmanagementbymohan24 <mohankasimkota2124@gmail.com>
2026-07-14 12:56:05 +0530

    Fix database name

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST", "localhost")
 app.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT", 3306))
 app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER", "root")
 app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD", "")
-app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB", "college_event_db")
+app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB", "college_event")
 
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
